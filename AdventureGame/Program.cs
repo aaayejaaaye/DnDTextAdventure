b34e0843442999db072cc1a4b1271caebcf891e8
aaayejaaaye <ajones369@student.cccs.edu>
2019-06-04 09:40:49 -0600

    added character creation or race and Ascii art for elf creation, added new class for itemsand character trait tracking

--- AdventureGame/Program.cs
+++ AdventureGame/Program.cs
@@ -6,6 +6,7 @@ namespace AdventureGame
     {
         static void Main()
         {
+  
             Game.StartGame();
             Console.ReadKey();
         }
@@ -17,20 +18,63 @@ namespace AdventureGame
             //print out game title
             public static void StartGame()
             {
-                Console.WriteLine("*****************************************************************************************************************");
-                Console.WriteLine("D&D One Shot!");
-                Console.WriteLine("Magical! Mystical! Adventure!");
-                Console.WriteLine("By AaayeJaaaye");
-                Console.WriteLine("*****************************************************************************************************************");
+                Console.WriteLine("********************************************************************************************************************************************* \n");
+                Console.WriteLine(@"
+ 
+
+      __  __           _   _           _ _   __  __             _           _ _               _                 _                  _ 
+     |  \/  |         | | (_)         | | | |  \/  |           (_)         | | |     /\      | |               | |                | |
+     | \  / |_   _ ___| |_ _  ___ __ _| | | | \  / | __ _  __ _ _  ___ __ _| | |    /  \   __| |_   _____ _ __ | |_ _   _ _ __ ___| |
+     | |\/| | | | / __| __| |/ __/ _` | | | | |\/| |/ _` |/ _` | |/ __/ _` | | |   / /\ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ \ |
+     | |  | | |_| \__ \ |_| | (_| (_| | |_| | |  | | (_| | (_| | | (_| (_| | |_|  / ____ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/_|
+     |_|  |_|\__, |___/\__|_|\___\__,_|_(_) |_|  |_|\__,_|\__, |_|\___\__,_|_(_) /_/    \_\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___(_)
+              __/ |                                        __/ |                                                                     
+             |___/                                        |___/                                                                      
+");
+                Console.WriteLine(@"      
+                                      _____         _____     ____  _   _ ______    _____ _           _   
+                                     |  __ \  ___  |  __ \   / __ \| \ | |  ____|  / ____| |         | |  
+                                     | |  | |( _ ) | |  | | | |  | |  \| | |__    | (___ | |__   ___ | |_ 
+                                     | |  | |/ _ \/\ |  | | | |  | | . ` |  __|    \___ \| '_ \ / _ \| __|
+                                     | |__| | (_>  < |__| | | |__| | |\  | |____   ____) | | | | (_) | |_ 
+                                     |_____/ \___/\/_____/   \____/|_| \_|______| |_____/|_| |_|\___/ \__|
+                                                                      
+                                                                      
+");
+   
+               
+                Console.WriteLine(@"  
+                                    .                  .-.    .  _   *     _   .
+                                               *          /   \     ((       _/ \       *    .
+                                             _    .   .--'\/\_ \     `      /    \  *    ___
+                                         *  / \_    _/ ^      \/\'__        /\/\  /\  __/   \ *
+                                           /    \  /    .'   _/  /  \  *' /    \/  \/ .`'\_/\   .
+                                      .   /\/\  /\/ :' __  ^/  ^/    `--./.'  ^  `-.\ _    _:\ _
+                                         /    \/  \  _/  \-' __/.' ^ _   \_   .'\   _/ \ .  __/ \
+                                       /\  .-   `. \/     \ / -.   _/ \ -. `_/   \ /    `._/  ^  \
+                                      /  `-.__ ^   / .-'.--'    . /    `--./ .-'  `-.  `-. `.  -  `.
+                                    @/        `.  / /      `-.   /  .-'   / .   .'   \    \  \  .-  \%
+                                    @&8jgs@@%% @)&@&(88&@.-_=_-=_-=_-=_-=_.8@% &@&&8(8%@%8)(8@%8 8%@)%
+                                    @88:::&(&8&&8:::::%&`.~-_~~-~~_~-~_~-~~=.'@(&%::::%@8&8)::&#@8::::
+                                    `::::::8%@@%:::::@%&8:`.=~~-.~~-.~~=..~'8::::::::&@8:::::&8:::::'
+                                     `::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.'
+
+
+");
+                Console.WriteLine("\n *********************************************************************************************************************************************");
+               
                 PlayerName();
+                CreateCharacter();
+
             }
             //ask player for a name, and save it
             public static void PlayerName()
             {
-                Console.WriteLine("Brave Patron of the arts to what do you call yeself?");
+                Dialog( "\n Brave Patron of the arts! To What do ye call yeself?", "blue");
                 characterName = Console.ReadLine();
                 // Console.WriteLine("I bid ye welcome " + characterName + " I wish ye great tidings on your journey ahead...");
                 Console.Write("I bid ye welcome "); Dialog(characterName, "magenta"); Console.Write( " I wish ye great tidings on your journey ahead...");
+                
                 Console.ReadKey();
             }
             public static void Dialog(string message, string color)
@@ -42,10 +86,56 @@ namespace AdventureGame
                 Console.Write(message);
                 Console.ResetColor();
             }
+            public static void CreateCharacter() {
+                String Input = "";
+                String Human = " Human";
+                String Elf = " Elf";
+                String Orc = " Orc"; 
+                Dialog("\n Choose your Race: \n [1]"+Human + "\n [2]" + Elf +"\n [3]" + Orc +"\n" , "blue");
+                Input = Console.ReadLine();
+                Dialog(characterName, "magenta"); Console.WriteLine( " You've chosen the Race of the ");
+                if (Input == "2") { Items.Elf(); }
+            }
+        
 
         }
     class Items {
+            public static void CharacterTraits() {
+                String Type;
+                String Race;
+                int HP;
+                int Charisma;
+                int Deterity;
+                int Constitution;
+                int intelligence;
+                int wisdom;
+                int Strength;
+                //String Cantrips[];
 
+
+            }
+            public static void Elf() {
+                Console.WriteLine(@"     ELF      
+.-----.
+ \ ' /   _/    )/
+- ( ) -('---''--)
+ / . \((()\^_^/)()
+  \\_\ (()_)-((()()
+   '- \ )/\._./(()
+     '/\/( X   ) \
+     (___)|___/ ) \
+          |.#_|(___)
+         /\    \ ( (_
+         \/\/\/\) \\
+         | / \ |
+         |(   \|
+        _|_)__|_\_
+        )...()...(
+         | (   \ |     
+      .-'__,)  (  \
+  mrf           '\_-,");
+
+            }
           
         }
     }
