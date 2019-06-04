d8ad89af475b1acd2a6a69a7297144b619beeffd
aaayejaaaye <ajones369@student.cccs.edu>
2019-06-04 08:14:54 -0600

    done with tut 1-7, added dialogue color changes and intro basic ASCII

--- AdventureGame/Program.cs
+++ AdventureGame/Program.cs
@@ -6,17 +6,47 @@ namespace AdventureGame
     {
         static void Main()
         {
-            string characterName = "";
-            Console.WriteLine("D&D One Shot!");
-            Console.WriteLine("A Magical! Mystical! Adventure!");
-            Console.WriteLine("By AaayeJaaaye");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Brave Patron of the arts to what do you call yeself?");
-            characterName = Console.ReadLine();
-            Console.WriteLine(" ");
-            Console.WriteLine("I bid you welcome " + characterName + "I wish ye great tidings on your journey ahead...");
+            Game.StartGame();
             Console.ReadKey();
+        }
+    public static class Game
+        {
+            //charcter name
+            static string characterName = "";
+
+            //print out game title
+            public static void StartGame()
+            {
+                Console.WriteLine("*****************************************************************************************************************");
+                Console.WriteLine("D&D One Shot!");
+                Console.WriteLine("Magical! Mystical! Adventure!");
+                Console.WriteLine("By AaayeJaaaye");
+                Console.WriteLine("*****************************************************************************************************************");
+                PlayerName();
+            }
+            //ask player for a name, and save it
+            public static void PlayerName()
+            {
+                Console.WriteLine("Brave Patron of the arts to what do you call yeself?");
+                characterName = Console.ReadLine();
+                // Console.WriteLine("I bid ye welcome " + characterName + " I wish ye great tidings on your journey ahead...");
+                Console.Write("I bid ye welcome "); Dialog(characterName, "magenta"); Console.Write( " I wish ye great tidings on your journey ahead...");
+                Console.ReadKey();
+            }
+            public static void Dialog(string message, string color)
+            {
+                if (color == "red ") { Console.ForegroundColor = ConsoleColor.DarkRed; }
+                if(color == "blue") { Console.ForegroundColor = ConsoleColor.DarkBlue; }
+                if (color == "green") { Console.ForegroundColor = ConsoleColor.DarkGreen; }
+                if (color == "magenta") { Console.ForegroundColor = ConsoleColor.DarkMagenta; }
+                Console.Write(message);
+                Console.ResetColor();
+            }
+
+        }
+    class Items {
+
+          
         }
     }
 }
